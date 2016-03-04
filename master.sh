5898a77ff60218ed3ec0b2f296c5a5342831736e
Nguyen Ba Cuong <cuongnb14@gmail.com>
2016-03-04 23:07:22 +0700

    add script config mesos on cluster

---
+++ master.sh
@@ -0,0 +1,35 @@
+# Setup node master have run slave
+HOST=192.168.122.51
+
+# config zookeper connection, for all hosts
+echo zk://$HOST:2181/mesos > /etc/mesos/zk
+
+# config zookeper on master node
+echo 1 > /etc/zookeeper/conf/myid # id for master server
+echo server.1=$HOST:2888:3888 >> /etc/zookeeper/conf/zoo.cfg # zookeper mapping master server
+
+# config mesos
+echo 1 > /etc/mesos-master/quorum 
+echo $HOST > /etc/mesos-master/ip 
+cp /etc/mesos-master/ip /etc/mesos-master/hostname
+
+# config marathon
+mkdir -p /etc/marathon/conf
+cp /etc/mesos-master/hostname /etc/marathon/conf
+cp /etc/mesos/zk /etc/marathon/conf/master
+cp /etc/marathon/conf/master /etc/marathon/conf/zk
+echo zk://$HOST:2181/marathon > /etc/marathon/conf/zk
+
+# Configure Service Init Rules and Restart Services
+restart zookeeper
+start mesos-master
+start marathon
+
+# Config slave
+echo $HOST > /etc/mesos-slave/ip
+cp /etc/mesos-slave/ip /etc/mesos-slave/hostname
+# fix error mesos no start
+rm -rf /tmp/mesos/
+
+restart mesos-slave
+
---
+++ slave.sh
@@ -0,0 +1,23 @@
+HOST=192.168.122.53
+ZK_HOST=192.168.122.51 
+
+# config zookeper connection, for all hosts
+echo zk://$ZK_HOST:2181/mesos > /etc/mesos/zk
+
+# stop mesos master, zookeeper, marathon on slave
+stop zookeeper
+echo manual > /etc/init/zookeeper.override
+stop mesos-master
+echo manual > /etc/init/mesos-master.override
+stop marathon
+echo manual > /etc/init/marathon.override
+
+# Config slave
+echo $HOST > /etc/mesos-slave/ip
+cp /etc/mesos-slave/ip /etc/mesos-slave/hostname
+
+
+# fix error mesos no start
+rm -rf /tmp/mesos/
+
+restart mesos-slave
