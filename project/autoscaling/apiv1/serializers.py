d4133e11bebdcc2b66ba7e0129514307aa17fb1c
Nguyen Ba Cuong <cuongnb14@gmail.com>
2016-03-12 21:49:40 +0700

    create serializers object (models and message)

--- project/autoscaling/apiv1/serializers.py
+++ project/autoscaling/apiv1/serializers.py
@@ -1,18 +1,34 @@
 from django.contrib.auth.models import User, Group
+from apiv1.models import *
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.ModelSerializer):
+# class UserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ('username', 'email', 'groups')
+
+class WebAppSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=256)
+    min_instances = serializers.IntegerField()
+    max_instances = serializers.IntegerField()
+    status = serializers.CharField(max_length=10)
+    port = serializers.IntegerField(allow_null=True)
+    cpus = serializers.FloatField(allow_null=True)
+    mem = serializers.FloatField(allow_null=True)
+    instances = serializers.IntegerField(allow_null=True)
+
+class DatabaseAppSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ('username', 'email', 'groups')
+        model = DatabaseApp
+        fields = ('id', 'host', 'port', 'mysql_version',)
 
-
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
+class PolicySerializer(serializers.ModelSerializer):
     class Meta:
-        model = Group
-        fields = ('url', 'name')
+        model = Policy
+        fields = ('id', 'metric_type', 'upper_threshold','lower_threshold','instances_out','instances_in','scale_up_wait','scale_down_wait','disabled')
 
-class MarathonAppSerializer(serializers.Serializer):
-    id_ = serializers.CharField(max_length=100)
-    name = serializers.CharField(max_length=100)
+class MessageSerializer(serializers.Serializer):
+    status = serializers.CharField(max_length=45)
+    message = serializers.CharField(max_length=256)
+   
