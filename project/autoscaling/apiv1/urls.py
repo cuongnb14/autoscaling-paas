90952744041e06d3e32fef9fd950e639d5d289d7
Nguyen Ba Cuong <cuongnb14@gmail.com>
2016-03-12 21:53:45 +0700

    add 'post /api/v1/apps' for create app

--- project/autoscaling/apiv1/urls.py
+++ project/autoscaling/apiv1/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^users$', views.User.as_view(), name='users'),
+    url(r'^apps(/(?P<app_name>\w*))?$', views.WebAppView.as_view(), name='apps'),
+    url(r'^apps(/(?P<app_name>\w.*)/policies)?$', views.PolicyView.as_view(), name='policy_app'),
 ]
--- project/autoscaling/apiv1/views.py
+++ project/autoscaling/apiv1/views.py
@@ -3,24 +3,90 @@ from rest_framework.generics import ListCreateAPIView
 from rest_framework.response import Response
 from rest_framework import authentication, permissions
 from django.contrib.auth import models
-import json 
-from apiv1.serializers import UserSerializer, MarathonAppSerializer
+from apiv1.models import *
+from apiv1.serializers import *
 from django.core import serializers
 from rest_framework import viewsets
-
-
+from apiv1.utils import *
+import json
 # Create your views here.
-class User(APIView):
-    """docstring for ClassName"""
-    def get(self, request):
-        users = models.User.objects.all()
-        serializer = UserSerializer(users, many=True)
+
+class WebAppView(APIView):
+    def get(self, request, app_name):
+        try:
+            marathon_client = get_marathon_client
+
+            if app_name:
+                app = request.user.webapp_set.get(name=app_name)
+                app = get_info_app(app, marathon_client)
+                serializer = WebAppSerializer(data=app)
+            else:
+                apps = request.user.webapp_set.all()
+                data = []
+                for app in apps:
+                    app = get_info_app(app, marathon_client)
+                    data.append(app)
+                serializer = WebAppSerializer(data=data, many=True)
+        except WebApp.DoesNotExist as e:
+            data = {"status": "error", "message": "app {} does not exist".format(app_name)}
+            serializer = MessageSerializer(data=data)
+        except Exception as e:
+            data = {"status": "error", "message": str(e)}
+            serializer = MessageSerializer(data=data)
+
+        if serializer.is_valid():
+            return Response(serializer.data)
+        return Response("Unserialize object!")
+
+    def post(self, request, app_name):
+        new_app = request.data
+        try:
+            app = WebApp()
+            app_name = new_app["name"]
+            if app_name.startswith(request.user.username):
+                if WebApp.objects.filter(name=app_name):
+                    data = {"status": "error", "message": "app name {} already existed".format(app_name)}
+
+                else:
+                    app.name = new_app["name"]
+                    app.min_instances = new_app["min_instances"]
+                    app.max_instances = new_app["max_instances"]
+                    app.user = request.user
+                    app.save()
+                    data = {"status": "success", "message": "create app {} success".format(app_name)}
+
+            else:
+                data = {"status": "error", "message": "app name must start with your's username_"}
+        except Exception as e:
+            data = {"status": "error", "message": str(e)}
+
+        serializer = MessageSerializer(data=data)
+        if serializer.is_valid():
+            return Response(serializer.data)
+        return Response("Unserialize object!")
+
+    def delete(self, request, app_name):
+        marathon_client = get_marathon_client()
+        app = WebApp.objects.filter(name=app_name).first()
+        if app is None:
+            data = {"status": "error", "message": "app {} does not exist".format(app_name)}
+        else:
+            app.delete()
+            data = {"status": "success", "message": "delete app {} success".format(app_name)}
+        serializer = MessageSerializer(data=data)
+        if serializer.is_valid():
+            return Response(serializer.data)
+        return Response("Unserialize object!")
+
+class PolicyView(APIView):
+
+    def get(self, request, app_name):
+        try:
+            app = WebApp.objects.filter(name=app_name)[0]
+            policies = app.policy_set.all()
+            serializer = PolicySerializer(policies, many=True)
+        except IndexError as e:
+            data = {"status": "error", "message": "app {} does not exist".format(app_name)}
+            serializer = MessageSerializer(data=data)
+            serializer.is_valid()
         return Response(serializer.data)
-
-    def post(self, request):
-        data = {"id": "1", "name": "bot"}
-        serializer = MarathonAppSerializer(data={"id_": "1", "name": "bot"})
-        serializer.is_valid()
-        return Response(serializer.data)
-
-
