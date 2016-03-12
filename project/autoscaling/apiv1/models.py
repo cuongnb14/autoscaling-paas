9b3518ce74ab257f0ea453d39d7db169b64a0c82
Nguyen Ba Cuong <cuongnb14@gmail.com>
2016-03-12 21:48:26 +0700

    add Setting and DatabaseApp model

--- project/autoscaling/apiv1/models.py
+++ project/autoscaling/apiv1/models.py
@@ -5,8 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class WebApp(TimeStampedModel):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=256)
-    apache_version = models.CharField(max_length=10)
+    name = models.CharField(max_length=30, unique=True)
     min_instances = models.IntegerField()
     max_instances = models.IntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -16,14 +15,25 @@ class WebApp(TimeStampedModel):
 
 class DatabaseApp(TimeStampedModel):
     id = models.AutoField(primary_key=True)
+    type_db = models.CharField(max_length=10)
     host = models.CharField(max_length=45)
     port = models.IntegerField()
+    username = models.CharField(max_length=45)
     root_password = models.CharField(max_length=60)
-    mysql_version = models.CharField(max_length=10)
+    version = models.CharField(max_length=10)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
         db_table = "autoscaling_database_app"
+
+class DatabaseImage(TimeStampedModel):
+    id = models.AutoField(primary_key=True)
+    type_db = models.CharField(max_length=10)
+    version = models.CharField(max_length=10)
+    image_name = models.CharField(max_length=45)
+
+    class Meta:
+        db_table = "autoscaling_database_image"
 
 class Policy(TimeStampedModel):
     id = models.AutoField(primary_key=True)
@@ -38,6 +48,12 @@ class Policy(TimeStampedModel):
     disabled = models.BooleanField(default=False)
     
     class Meta:
-        db_table = "policies"
+        db_table = "autoscaling_policies"
 
-        
+class Setting(TimeStampedModel):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30, unique=True)
+    value = models.CharField(max_length=256)
+
+    class Meta:
+        db_table = "autoscaling_setting"         
