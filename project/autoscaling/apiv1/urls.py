246f3f60d4336009bb22569bb5e63e144b5051fc
Nguyen Ba Cuong <cuongnb14@gmail.com>
2016-03-13 21:33:36 +0700

    app name can character , number, _ or -

--- project/autoscaling/apiv1/urls.py
+++ project/autoscaling/apiv1/urls.py
@@ -2,8 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # /apps,    /apps/<app_name>
-    url(r'^apps(/(?P<app_name>\w*))?$', views.WebAppView.as_view(), name='apps'),
-    # /apps/<app_name>/policies,    /apps/<app_name>/policies/<policy_id>
-    url(r'^apps(/(?P<app_name>\w.*)/policies(/(?P<policy_id>\w*))?)?$', views.PolicyView.as_view(), name='policy_app'),
+    url(r'^apps(/(?P<app_name>[\w_0-9\-]*))?$', views.WebAppView.as_view(), name='apps'),
+    url(r'^apps(/(?P<app_name>[\w_0-9\-]*)/policies(/(?P<policy_id>\w*))?)?$', views.PolicyView.as_view(), name='policy_app'),
 ]
