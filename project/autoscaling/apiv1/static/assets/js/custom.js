8e986ce98a5060add55e4be7ee9dac1bac7cf76a
Nguyen Ba Cuong <cuongnb14@gmail.com>
2016-03-26 00:17:21 +0700

    move static file to locahost

---
+++ project/autoscaling/apiv1/static/assets/js/custom.js
@@ -0,0 +1,18 @@
+// //SHow password
+//
+// $( document ).ready(function() {
+//   $("#passwordfield").on("keyup",function(){
+//   if($(this).val())
+//       $(".btn-eye").show();
+//   else
+//       $(".btn-eye").hide();
+//   });
+//   $(".btn-eye").mousedown(function(){
+//     alert(1);
+//         $("#passwordfield").attr('type','text');
+//     }).mouseup(function(){
+//       $("#passwordfield").attr('type','password');
+//     }).mouseout(function(){
+//       $("#passwordfield").attr('type','password');
+//     });
+// });
