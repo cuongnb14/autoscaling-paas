110291d3bdd9227a65e92f0a5eaeef9fd386bc10
Nguyen Ba Cuong <cuongnb14@gmail.com>
2016-03-25 17:25:24 +0700

    update sericalizers env field

--- project/autoscaling/apiv1/utils.py
+++ project/autoscaling/apiv1/utils.py
@@ -1,6 +1,7 @@
 from apiv1.models import Setting
-from marathon import MarathonClient
+from marathon import MarathonClient, MarathonApp
 import traceback
+import json
 
 def get_info_app(app, marathon_client):
     temp = {}
@@ -8,6 +9,13 @@ def get_info_app(app, marathon_client):
     temp['github_url'] = app.github_url
     temp['min_instances'] = app.min_instances
     temp['max_instances'] = app.max_instances
+    temp['env_hostname'] = app.env_hostname
+    temp['env_port'] = app.env_port
+    temp['env_db_hostname'] = app.env_db_hostname
+    temp['env_db_port'] = app.env_db_port
+    temp['env_db_name'] = app.env_db_name
+    temp['env_db_username'] = app.env_db_username
+    temp['env_db_password'] = app.env_db_password
     temp['status'] = app.status
     try:
         marathon_app = marathon_client.get_app("{}.{}".format(app.user.username,app.name))
@@ -23,7 +31,7 @@ def get_info_app(app, marathon_client):
         temp['instances'] = 0
     return temp
 
-def get_setting(key, default):
+def get_setting(key, default=None):
     setting = Setting.objects.filter(name=key).first()
     if setting:
         return setting.value
@@ -37,6 +45,13 @@ def get_marathon_client():
         return marathon_client
     except Exception as e:
         raise Exception("unconnect to marathon")
+
+def get_marathon_app(app_json):
+    app_dict = json.loads(app_json)
+    app_marathon = MarathonApp
+    for key, value in app_dict.items():
+        setattr(app_marathon, key, value)
+    return app_marathon
 
 def get_message_serializer(status, message):
     data = {"status": status, "message": message}
