231db04b13382f9d2e1b12f7e0c465dc66376e1a
Nguyen Ba Cuong <cuongnb14@gmail.com>
2016-04-01 12:00:15 +0700

    cut module modal to new file

--- project/autoscaling/apiv1/static/app/controllers/database.js
+++ project/autoscaling/apiv1/static/app/controllers/database.js
@@ -1,39 +1,5 @@
 'use strict';
 
-var modal = angular.module('ABModal');
-
-modal.controller('ModalController', function ($scope) {
-    var mc = this;
-    mc.showModal = false;
-    mc.showModal = function(name){
-        $('#'+name).modal('show')
-    };
-  });
-
-modal.directive('modal', function () {
-    return {
-      template: '<div class="modal fade" tabindex="-1" role="dialog">' +
-          '<div class="modal-dialog">' +
-            '<div class="modal-content">' +
-              '<div class="modal-header">' +
-                '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
-                '<h4 class="modal-title">{{ title }}</h4>' +
-              '</div>' +
-              '<div class="modal-body" ng-transclude></div>' +
-            '</div>' +
-          '</div>' +
-        '</div>',
-      restrict: 'E',
-      transclude: true,
-      replace:true,
-      scope:true,
-      link: function postLink(scope, element, attrs) {
-        scope.title = attrs.title;
-      }
-    };
-  });
-
-
 angular.module('Database')
 
 .controller('GetDatabasesController',
