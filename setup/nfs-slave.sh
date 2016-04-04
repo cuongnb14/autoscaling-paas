a0b17d6d89edfda4106a2d9f28d8b5b67aca9a6f
Nguyen Ba Cuong <cuongnb14@gmail.com>
2016-04-04 12:05:55 +0700

    run command startup nfs in deamon

--- setup/nfs-slave.sh
+++ setup/nfs-slave.sh
@@ -7,4 +7,4 @@ sudo dpkg -i docker-volume-netshare_0.12_i386.deb
 
 # run on startup
 sudo sed -i "4iservice docker-volume-netshare start" /etc/rc.local
-sudo sed -i "5isudo docker-volume-netshare nfs" /etc/rc.local
+sudo sed -i "5isudo docker-volume-netshare nfs &" /etc/rc.local
