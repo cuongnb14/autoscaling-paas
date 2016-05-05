c953d676502bd629a830a94ad7bc57bd369acf3b
Nguyen Ba Cuong <cuongnb14@gmail.com>
2016-05-06 01:05:08 +0700

    change strategy scale

--- project/autoscaling/autoscaling/decider.py
+++ project/autoscaling/autoscaling/decider.py
@@ -51,11 +51,10 @@ class BaseRuleDecider(Decider):
             if(instances_delta['up'] < delta['up']):
                 instances_delta['up'] = delta['up']
                 time_wait["up"] = time_wait_temp["up"]
-            if(instances_delta['down'] > delta['down'] and delta['down'] != 0):
+            if(instances_delta['down'] > delta['down']):
                 instances_delta['down'] = delta['down']
                 time_wait["down"] = time_wait_temp["down"]
-        if instances_delta['down'] == 100:
-            instances_delta['down'] = 0
+
 
         if(instances_delta['up'] > 0):
             self.scaler.scale(instances_delta['up'])
